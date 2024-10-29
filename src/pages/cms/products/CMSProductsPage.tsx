7d4af68af534d64f2d560eb54eab302fadeb8579
Manuel Morandin <manuel.morandin@gmail.com>
2024-10-29 23:25:06 +1100

    New version

--- src/pages/cms/products/CMSProductsPage.tsx
+++ src/pages/cms/products/CMSProductsPage.tsx
@@ -1,9 +1,20 @@
+import { useProductsService } from "@/services/products";
+import { useEffect } from "react";
+
 export function CMSProductsPage() {
+  const { state, actions } = useProductsService();
+
+  useEffect(() => {
+    actions.getProducts();
+  }, []);
+
   return (
     <div>
       <h1 className="title">CMS</h1>
-
       Pagina Prodotti
+      {state.pending && <div>loading...</div>}
+      {state.error && <div>error!!!!</div>}
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
-  )
+  );
 }
