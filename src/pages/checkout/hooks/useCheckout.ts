7dee4841cedd10eec2aafc0919ead0a25404dcc6
Manuel Morandin <manuel.morandin@gmail.com>
2024-10-31 22:07:25 +1100

    initial commit

--- src/pages/checkout/hooks/useCheckout.ts
+++ src/pages/checkout/hooks/useCheckout.ts
@@ -1,24 +1,27 @@
-import { OrderForm } from '@/model/order-form';
-import { selectCartList, selectTotalCartCost, useCart } from '@/services/cart';
-import React, { ChangeEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { OrderForm } from "@/model/order-form";
+import { selectCartList, selectTotalCartCost, useCart } from "@/services/cart";
+import { useOrdersService } from "@/services/orders";
+import React, { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const EMAIL_REGEX =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export function useCheckout() {
   const navigate = useNavigate();
-  const [user, setUser] = useState({ name: '', email: ''});
+  const [user, setUser] = useState({ name: "", email: "" });
   const [dirty, setDirty] = useState(false);
 
   const totalCartCost = useCart(selectTotalCartCost);
-  const clearCart = useCart(state => state.clearCart);
+  const clearCart = useCart((state) => state.clearCart);
 
+  const { addOrder } = useOrdersService();
   const order = useCart(selectCartList);
 
   function changeHandler(e: ChangeEvent<HTMLInputElement>) {
     const name = e.currentTarget.name;
     const value = e.currentTarget.value;
-    setUser(state => ({ ...state, [name]: value }));
+    setUser((state) => ({ ...state, [name]: value }));
     setDirty(true);
   }
 
@@ -27,13 +30,14 @@ export function useCheckout() {
     const orderInfo: OrderForm = {
       user,
       order,
-      status: 'pending',
-      total: totalCartCost
-    }
-    console.log(orderInfo)
+      status: "pending",
+      total: totalCartCost,
+    };
+    console.log(orderInfo);
+    addOrder(orderInfo);
 
     clearCart();
-    navigate('/thankyou');
+    navigate("/thankyou");
   }
 
   const isNameValid = user.name.length;
@@ -52,6 +56,6 @@ export function useCheckout() {
     },
     user,
     dirty,
-    totalCartCost
-  }
+    totalCartCost,
+  };
 }
--- src/pages/cms/products/CMSProductsPage.tsx
+++ src/pages/cms/products/CMSProductsPage.tsx
@@ -1,5 +1,8 @@
-import { useProductsService } from "@/services/products";
 import { useEffect } from "react";
+import { useProductsService } from "@/services/products";
+import { ServerError, Spinner } from "@/shared/";
+import { CMSProductForm } from "./components/CMSProductForm";
+import { CMSProductsList } from "./components/CMSProductsList";
 
 export function CMSProductsPage() {
   const { state, actions } = useProductsService();
@@ -11,10 +14,27 @@ export function CMSProductsPage() {
   return (
     <div>
       <h1 className="title">CMS</h1>
-      Pagina Prodotti
-      {state.pending && <div>loading...</div>}
-      {state.error && <div>error!!!!</div>}
-      <pre>{JSON.stringify(state, null, 2)}</pre>
+
+      {state.pending && <Spinner />}
+      {state.error && <ServerError message={state.error} />}
+
+      <CMSProductForm
+        activeItem={state.activeItem}
+        onClose={actions.resetActiveItem}
+        onAdd={actions.addProduct}
+        onEdit={actions.editProduct}
+      />
+
+      <CMSProductsList
+        items={state.products}
+        activeItem={state.activeItem}
+        onEditItem={actions.setActiveItem}
+        onDeleteItem={actions.deleteProduct}
+      />
+
+      <button className="btn primary" onClick={() => actions.setActiveItem({})}>
+        ADD NEW
+      </button>
     </div>
   );
 }
